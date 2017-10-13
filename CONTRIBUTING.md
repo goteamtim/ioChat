c25da79249afb486a569374154d1d32a37dc22da
Casey Provost <casey.provost@spreemo.com>
2017-10-13 11:23:48 -0400

    refactored es5 to es6 where appropriate, added build step for compatibility with gulp, pulled out most remote dependencies and made them local, and finally re-organized the app using the industry standard pattern of "dist" and "src" folders.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -14,8 +14,8 @@ Steps to follow to contribute
 
 - Fork the repo
 - run ```npm install``` to install the dependencies.
-- You might want to install nodemon (npm install nodemon) so as to see your changes applied on the fly.
-- run ```npm start``` or (```npm run start:dev``` if you installed nodemon) to start the development server
+- install gulp globally if you haven't ```npm install --global gulp-cli```
+- run ```gulp``` to start the development server
 - Point your browser to ```localhost:3000``` to fireup the application.
 - Address an issue and raise a PR, stating the issue you addressed with a screenshot of the new solution.
 
---
+++ src/server/server.js
@@ -0,0 +1,49 @@
+'use strict';
+
+import express from 'express';
+import http from 'http';
+import SocketIO from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new SocketIO(server);
+let users = [];
+let connections = [];
+
+server.listen(process.env.PORT || 3000);
+
+console.log('Server is presently running....');
+
+app.use(express['static'](__dirname + '/../client'));
+
+io.sockets.on('connection', (socket) => {
+  connections.push(socket);
+  console.log('Connected: %s connection(s) currently active.', connections.length);
+
+  // Disconnect
+  socket.on('disconnect', (data) => {
+    if (!socket.username) return;
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsername();
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected: %s connection(s) currently active.', connections.length);
+  });
+
+  // Message
+  socket.on('send message', (data) => {
+    console.log(data);
+    io.sockets.emit('new message', { msg: data, user: socket.username });
+  });
+  // User
+
+  socket.on('new user', (data, callback) => {
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsername();
+  });
+
+  function updateUsername() {
+    io.sockets.emit('get users', users);
+  }
+});
