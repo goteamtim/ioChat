e1b84e82636af352bafb5218a23cc2fbf47fe2b1
Bill <billmike1994@gmail.com>
2017-10-13 07:51:25 +0100

    Refactor server.js

--- server.js
+++ server.js
@@ -14,9 +14,11 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 io.sockets.on('connection', (socket) => {
     connections.push(socket);
-    console.log('Connected: %s connections are currently active.', connections.length);
+    console.log('Connected: %s connection(s) currently active.', connections.length);
 
     // Disconnect
-    connections.splice(connections.indexOf(socket), 1);
-    console.log('Disconnected: %s connectoins are currently active.', connections.length);
+    socket.on('disconnect', (data) => {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s connection(s) currently active.', connections.length);
+    });
 });
